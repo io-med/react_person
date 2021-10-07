101d391582e04e98c9983b05ea4bbbf5b8cb8a70
Iurii Medvedchuk <io.medvedchuk@gmail.com>
2021-10-07 21:30:39 +0300

    Updates

---
+++ precache-manifest.95fc47101000dba9c50891ee1d96b798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8284bdf38e7d06e3325f38e1ef0ee94",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec099ee9d5b3dfa3b753",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec099ee9d5b3dfa3b753",
+    "url": "./static/js/main.d2e67b08.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
